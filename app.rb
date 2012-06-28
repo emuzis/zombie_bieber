53928c06a86f27a1852c839bc74569ab348a5fc1
Peter Wu <peter.wu@xbridge.com>
2012-06-28 16:47:26 +0100

    tiny up some constants

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'grid'
 
 IMG_HEIGHT  = 48
-IMG_WIDTH   = 48
+IMG_WIDTH   = 100
 BORDER      = 1
 ROW_NUM     = 10
 COL_NUM     = 10
@@ -9,12 +9,12 @@ MARGIN      = IMG_HEIGHT / 2
 APP_HEIGHT  = (IMG_HEIGHT + BORDER * 2) * ROW_NUM + MARGIN * 2
 APP_WIDTH   = (IMG_WIDTH + BORDER * 2) * COL_NUM + MARGIN * 2
 
-BLOCK_SIZE  = (IMG_HEIGHT) + (BORDER * 2)
+BLOCK_SIZE  = { :x => (IMG_WIDTH) + (BORDER * 2), :y => (IMG_HEIGHT) + (BORDER * 2) }
 
 TOP_LEFT      = { :x => MARGIN, :y => MARGIN }
-TOP_RIGHT     = { :x => APP_HEIGHT - MARGIN, :y => MARGIN }
-BOTTOM_RIGHT  = { :x => APP_HEIGHT - MARGIN, :y => APP_WIDTH - MARGIN }
-BOTTOM_LEFT   = { :x => MARGIN, :y => APP_WIDTH - MARGIN }
+TOP_RIGHT     = { :x => APP_WIDTH - MARGIN, :y => MARGIN }
+BOTTOM_RIGHT  = { :x => APP_WIDTH - MARGIN, :y => APP_HEIGHT - MARGIN }
+BOTTOM_LEFT   = { :x => MARGIN, :y => APP_HEIGHT - MARGIN }
 
 BIEBER = "bieber2.jpeg"
 FINISH = "finish.jpeg"
@@ -29,25 +29,25 @@ Shoes.app :height => APP_HEIGHT, :width => APP_WIDTH do
     @@end_point   = { :x => rand(COL_NUM), :y => rand(ROW_NUM) }
     
     finish_flag = stack { image FINISH, :width => IMG_WIDTH, :height => IMG_HEIGHT }
-    finish_flag.move((TOP_LEFT[:x] + @@end_point[:x] * BLOCK_SIZE), (TOP_LEFT[:y] + @@end_point[:y] * BLOCK_SIZE))
+    finish_flag.move((TOP_LEFT[:x] + @@end_point[:x] * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + @@end_point[:y] * BLOCK_SIZE[:y]))
     
     @@grid.each_with_index do |row, y|
       row.each_with_index do |box, x|
         if box[:top]
-          line((TOP_LEFT[:x] + x * BLOCK_SIZE), (TOP_LEFT[:y] + y * BLOCK_SIZE), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE), (TOP_LEFT[:y] + y * BLOCK_SIZE))
-          line((TOP_LEFT[:x] + x * BLOCK_SIZE), (TOP_LEFT[:y] + y * BLOCK_SIZE)-1, (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE), (TOP_LEFT[:y] + y * BLOCK_SIZE)-1)
+          line((TOP_LEFT[:x] + x * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + y * BLOCK_SIZE[:y]), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + y * BLOCK_SIZE[:y]))
+          line((TOP_LEFT[:x] + x * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + y * BLOCK_SIZE[:y])-1, (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + y * BLOCK_SIZE[:y])-1)
         end
         if box[:bottom]
-          line((TOP_LEFT[:x] + x * BLOCK_SIZE), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE))
-          line((TOP_LEFT[:x] + x * BLOCK_SIZE), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE)-1, (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE)-1)
+          line((TOP_LEFT[:x] + x * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y]), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y]))
+          line((TOP_LEFT[:x] + x * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y])-1, (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y])-1)
         end
         if box[:left]
-          line((TOP_LEFT[:x] + x * BLOCK_SIZE), (TOP_LEFT[:y] + y * BLOCK_SIZE), (TOP_LEFT[:x] + (x) * BLOCK_SIZE), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE))
-          line((TOP_LEFT[:x] + x * BLOCK_SIZE)-1, (TOP_LEFT[:y] + y * BLOCK_SIZE), (TOP_LEFT[:x] + (x) * BLOCK_SIZE)-1, (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE))
+          line((TOP_LEFT[:x] + x * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + y * BLOCK_SIZE[:y]), (TOP_LEFT[:x] + (x) * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y]))
+          line((TOP_LEFT[:x] + x * BLOCK_SIZE[:x])-1, (TOP_LEFT[:y] + y * BLOCK_SIZE[:y]), (TOP_LEFT[:x] + (x) * BLOCK_SIZE[:x])-1, (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y]))
         end
         if box[:right]
-          line((TOP_LEFT[:x] + (x+ 1) * BLOCK_SIZE), (TOP_LEFT[:y] + y * BLOCK_SIZE), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE))
-          line((TOP_LEFT[:x] + (x+ 1) * BLOCK_SIZE)-1, (TOP_LEFT[:y] + y * BLOCK_SIZE), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE)-1, (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE))
+          line((TOP_LEFT[:x] + (x+ 1) * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + y * BLOCK_SIZE[:y]), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE[:x]), (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y]))
+          line((TOP_LEFT[:x] + (x+ 1) * BLOCK_SIZE[:x])-1, (TOP_LEFT[:y] + y * BLOCK_SIZE[:y]), (TOP_LEFT[:x] + (x + 1) * BLOCK_SIZE[:x])-1, (TOP_LEFT[:y] + (y + 1) * BLOCK_SIZE[:y]))
         end
       end
     end
@@ -63,8 +63,8 @@ end
 
 def check_path x, y
   value = {}
-  value[:x] = (x - MARGIN - 1) / BLOCK_SIZE
-  value[:y] = (y - MARGIN - 1) / BLOCK_SIZE
+  value[:x] = (x - MARGIN - 1) / BLOCK_SIZE[:x]
+  value[:y] = (y - MARGIN - 1) / BLOCK_SIZE[:y]
   value
 end
 
@@ -73,13 +73,13 @@ def moving (object, direction)
   cell = @@grid[position_before[:y]][position_before[:x]]
   case direction
   when :up
-    object.move(@@cursor[:x], @@cursor[:y] -= BLOCK_SIZE) if @@cursor[:y] - BLOCK_SIZE > 0 if !cell[:top]
+    object.move(@@cursor[:x], @@cursor[:y] -= BLOCK_SIZE[:y]) if @@cursor[:y] - BLOCK_SIZE[:y] > 0 if !cell[:top]
   when :down
-    object.move(@@cursor[:x], @@cursor[:y] += BLOCK_SIZE) if @@cursor[:y] + BLOCK_SIZE < APP_HEIGHT - (MARGIN + IMG_HEIGHT - 1) if !cell[:bottom]
+    object.move(@@cursor[:x], @@cursor[:y] += BLOCK_SIZE[:y]) if @@cursor[:y] + BLOCK_SIZE[:y] < APP_HEIGHT - (MARGIN + IMG_HEIGHT - 1) if !cell[:bottom]
   when :right
-    object.move(@@cursor[:x] += BLOCK_SIZE, @@cursor[:y]) if @@cursor[:x] + BLOCK_SIZE < APP_WIDTH - (MARGIN + IMG_WIDTH - 1) if !cell[:right]
+    object.move(@@cursor[:x] += BLOCK_SIZE[:x], @@cursor[:y]) if @@cursor[:x] + BLOCK_SIZE[:x] < APP_WIDTH - (MARGIN + IMG_WIDTH - 1) if !cell[:right]
   when :left
-    object.move(@@cursor[:x] -= BLOCK_SIZE, @@cursor[:y]) if @@cursor[:x] - BLOCK_SIZE > 0 if !cell[:left]
+    object.move(@@cursor[:x] -= BLOCK_SIZE[:x], @@cursor[:y]) if @@cursor[:x] - BLOCK_SIZE[:x] > 0 if !cell[:left]
   end
   position_after = check_path(@@cursor[:x], @@cursor[:y])
   @@steps_count += 1 if position_before != position_after
