c8c69cdde5ed680a6654dc5ce582d7536d2ee1bf
Egils Muzis <egils.muzis@xbridge.com>
2012-06-29 15:19:25 +0100

    Use strokewidth instead of always drawing 2 lines

--- app.rb
+++ app.rb
@@ -1,15 +1,11 @@
 require 'grid'
 require 'socket'
 
-IMG_HEIGHT  = 48
-IMG_WIDTH   = 48
 BORDER      = 1
-EASY        = {:row => 4,   :col => 4}
-MEDIUM      = {:row => 10,  :col => 10}
-HARD        = {:row => 15,  :col => 15}
-MARGIN      = IMG_HEIGHT / 2
-
-BLOCK_SIZE  = { :x => (IMG_WIDTH) + (BORDER * 2), :y => (IMG_HEIGHT) + (BORDER * 2) }
+EASY        = {:row => 4,   :col => 4, :img_height => 48, :img_width => 48}
+MEDIUM      = {:row => 10,  :col => 10, :img_height => 48, :img_width => 48}
+HARD        = {:row => 15,  :col => 15, :img_height => 48, :img_width => 48}
+JOSS        = {:row => 60,  :col => 60, :img_height => 10, :img_width => 10}
 
 BIEBER    = "bieber2.jpeg"
 FINISH    = "zombieber.png"
@@ -34,6 +30,14 @@ def landing_page parent_window, mode
         @option = list.text
       end
     }
+    
+    keypress do |k|
+      if k.to_s == "j"
+        start_game :joss
+        close
+      end
+    end
+    
     start_btn = stack {
       button "Start!!" do
         start_game @option
@@ -52,49 +56,52 @@ end
 
 def start_game size
   
+  @@img_height = instance_eval(size.to_s.upcase)[:img_height]
+  @@img_width = instance_eval(size.to_s.upcase)[:img_width]
+  
+  @@margin = @@img_height / 2
+  @@block_size = { :x => (@@img_width) + (BORDER * 2), :y => (@@img_height) + (BORDER * 2) }
+  
   @@row_num = instance_eval(size.to_s.upcase)[:row]
   @@col_num = instance_eval(size.to_s.upcase)[:col]
   
-  @@app_height    = (IMG_HEIGHT + BORDER * 2) * @@row_num + MARGIN * 2
-  @@app_width     = (IMG_WIDTH + BORDER * 2) * @@col_num + MARGIN * 2
-  top_left      = { :x => MARGIN, :y => MARGIN }
-  top_right     = { :x => @@app_width - MARGIN, :y => MARGIN }
-  bottom_right  = { :x => @@app_width - MARGIN, :y => @@app_height - MARGIN }
-  bottom_left   = { :x => MARGIN, :y => @@app_height - MARGIN }
+  @@app_height    = (@@img_height + BORDER * 2) * @@row_num + @@margin * 2
+  @@app_width     = (@@img_width + BORDER * 2) * @@col_num + @@margin * 2
+  top_left      = { :x => @@margin, :y => @@margin }
+  top_right     = { :x => @@app_width - @@margin, :y => @@margin }
+  bottom_right  = { :x => @@app_width - @@margin, :y => @@app_height - @@margin }
+  bottom_left   = { :x => @@margin, :y => @@app_height - @@margin }
   Shoes.app(:height => @@app_height, :width => @@app_width, :title => "Zombieber", :resizable => false) do
     background white
+    strokewidth(2)
     s = stack :width => @@app_width, :height => @@app_height do
-      @@cursor      = { :x => MARGIN + BORDER, :y => MARGIN + BORDER }
+      @@cursor      = { :x => @@margin + BORDER, :y => @@margin + BORDER }
       @@grid        = Grid.new(@@row_num,@@col_num).grid_hash
       @@start_time  = Time.now
       @@steps_count = 0
       @@end_point   = { :x => rand(@@col_num / 2) + @@col_num / 2, :y => rand(@@row_num / 2) + @@row_num / 2}
     
-      finish_flag = stack { image FINISH, :width => IMG_WIDTH, :height => IMG_HEIGHT }
-      finish_flag.move((top_left[:x] + @@end_point[:x] * BLOCK_SIZE[:x]), (top_left[:y] + @@end_point[:y] * BLOCK_SIZE[:y]))
+      finish_flag = stack { image FINISH, :width => @@img_width, :height => @@img_height }
+      finish_flag.move((top_left[:x] + @@end_point[:x] * @@block_size[:x]), (top_left[:y] + @@end_point[:y] * @@block_size[:y]))
     
       @@grid.each_with_index do |row, y|
         row.each_with_index do |box, x|
           if box[:top]
-            line((top_left[:x] + x * BLOCK_SIZE[:x]), (top_left[:y] + y * BLOCK_SIZE[:y]), (top_left[:x] + (x + 1) * BLOCK_SIZE[:x]), (top_left[:y] + y * BLOCK_SIZE[:y]))
-            line((top_left[:x] + x * BLOCK_SIZE[:x]), (top_left[:y] + y * BLOCK_SIZE[:y])-1, (top_left[:x] + (x + 1) * BLOCK_SIZE[:x]), (top_left[:y] + y * BLOCK_SIZE[:y])-1)
+            line((top_left[:x] + x * @@block_size[:x]), (top_left[:y] + y * @@block_size[:y]), (top_left[:x] + (x + 1) * @@block_size[:x]), (top_left[:y] + y * @@block_size[:y]))
           end
           if box[:bottom]
-            line((top_left[:x] + x * BLOCK_SIZE[:x]), (top_left[:y] + (y + 1) * BLOCK_SIZE[:y]), (top_left[:x] + (x + 1) * BLOCK_SIZE[:x]), (top_left[:y] + (y + 1) * BLOCK_SIZE[:y]))
-            line((top_left[:x] + x * BLOCK_SIZE[:x]), (top_left[:y] + (y + 1) * BLOCK_SIZE[:y])-1, (top_left[:x] + (x + 1) * BLOCK_SIZE[:x]), (top_left[:y] + (y + 1) * BLOCK_SIZE[:y])-1)
+            line((top_left[:x] + x * @@block_size[:x]), (top_left[:y] + (y + 1) * @@block_size[:y]), (top_left[:x] + (x + 1) * @@block_size[:x]), (top_left[:y] + (y + 1) * @@block_size[:y]))
           end
           if box[:left]
-            line((top_left[:x] + x * BLOCK_SIZE[:x]), (top_left[:y] + y * BLOCK_SIZE[:y]), (top_left[:x] + (x) * BLOCK_SIZE[:x]), (top_left[:y] + (y + 1) * BLOCK_SIZE[:y]))
-            line((top_left[:x] + x * BLOCK_SIZE[:x])-1, (top_left[:y] + y * BLOCK_SIZE[:y]), (top_left[:x] + (x) * BLOCK_SIZE[:x])-1, (top_left[:y] + (y + 1) * BLOCK_SIZE[:y]))
+            line((top_left[:x] + x * @@block_size[:x]), (top_left[:y] + y * @@block_size[:y]), (top_left[:x] + (x) * @@block_size[:x]), (top_left[:y] + (y + 1) * @@block_size[:y]))
           end
           if box[:right]
-            line((top_left[:x] + (x+ 1) * BLOCK_SIZE[:x]), (top_left[:y] + y * BLOCK_SIZE[:y]), (top_left[:x] + (x + 1) * BLOCK_SIZE[:x]), (top_left[:y] + (y + 1) * BLOCK_SIZE[:y]))
-            line((top_left[:x] + (x+ 1) * BLOCK_SIZE[:x])-1, (top_left[:y] + y * BLOCK_SIZE[:y]), (top_left[:x] + (x + 1) * BLOCK_SIZE[:x])-1, (top_left[:y] + (y + 1) * BLOCK_SIZE[:y]))
+            line((top_left[:x] + (x+ 1) * @@block_size[:x]), (top_left[:y] + y * @@block_size[:y]), (top_left[:x] + (x + 1) * @@block_size[:x]), (top_left[:y] + (y + 1) * @@block_size[:y]))
           end
         end
       end
   
-      @bieber = stack { image BIEBER, :width => IMG_WIDTH, :height => IMG_HEIGHT }
+      @bieber = stack { image BIEBER, :width => @@img_width, :height => @@img_height }
       @bieber.move(@@cursor[:x], @@cursor[:y])
     
       keypress do |k|
@@ -129,8 +136,8 @@ landing_page nil, :welcome
 
 def check_path x, y
   value = {}
-  value[:x] = (x - MARGIN - 1) / BLOCK_SIZE[:x]
-  value[:y] = (y - MARGIN - 1) / BLOCK_SIZE[:y]
+  value[:x] = (x - @@margin - 1) / @@block_size[:x]
+  value[:y] = (y - @@margin - 1) / @@block_size[:y]
   value
 end
 
@@ -139,13 +146,13 @@ def moving (object, direction)
   cell = @@grid[position_before[:y]][position_before[:x]]
   case direction
   when :up
-    object.move(@@cursor[:x], @@cursor[:y] -= BLOCK_SIZE[:y]) if @@cursor[:y] - BLOCK_SIZE[:y] > 0 if !cell[:top]
+    object.move(@@cursor[:x], @@cursor[:y] -= @@block_size[:y]) if @@cursor[:y] - @@block_size[:y] > 0 if !cell[:top]
   when :down
-    object.move(@@cursor[:x], @@cursor[:y] += BLOCK_SIZE[:y]) if @@cursor[:y] + BLOCK_SIZE[:y] < @@app_height - (MARGIN + IMG_HEIGHT - 1) if !cell[:bottom]
+    object.move(@@cursor[:x], @@cursor[:y] += @@block_size[:y]) if @@cursor[:y] + @@block_size[:y] < @@app_height - (@@margin + @@img_height - 1) if !cell[:bottom]
   when :right
-    object.move(@@cursor[:x] += BLOCK_SIZE[:x], @@cursor[:y]) if @@cursor[:x] + BLOCK_SIZE[:x] < @@app_width - (MARGIN + IMG_WIDTH - 1) if !cell[:right]
+    object.move(@@cursor[:x] += @@block_size[:x], @@cursor[:y]) if @@cursor[:x] + @@block_size[:x] < @@app_width - (@@margin + @@img_width - 1) if !cell[:right]
   when :left
-    object.move(@@cursor[:x] -= BLOCK_SIZE[:x], @@cursor[:y]) if @@cursor[:x] - BLOCK_SIZE[:x] > 0 if !cell[:left]
+    object.move(@@cursor[:x] -= @@block_size[:x], @@cursor[:y]) if @@cursor[:x] - @@block_size[:x] > 0 if !cell[:left]
   end
   position_after = check_path(@@cursor[:x], @@cursor[:y])
   @@steps_count += 1 if position_before != position_after
